76619f96c96ae06f09c69790e70e31ab30115cf9
jonathanReis <jobathansantos@bol.com.br>
2014-12-02 23:13:51 -0200

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,28 @@
+var __$ = function(id)
+{
+	if(id)
+	{
+		return document.querySelectorAll(id);
+	}
+	return document;
+};
+
+var run = function()
+{
+	var els = __$('*[inject]');
+	var re = /#{([a-zA-Z.]+)}/g; 
+	var m;
+	for(x in els)
+	{
+		if(els[x] instanceof Element)
+		{
+			m = re.exec(els[x].innerHTML);
+			while (els[x].innerHTML.match(re)) {
+	    		els[x].innerHTML = els[x].innerHTML.replace(eval('/#{' + m[1] + '}/g'), eval(m[1]));
+	    		m = re.exec(els[x].innerHTML);
+			}
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', run, false);
